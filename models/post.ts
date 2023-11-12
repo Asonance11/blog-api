0aacea666b726c5305eddfe96fa99660ce579592
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-12 13:40:09 +0100

    Add Post model with required fields and timestamps

---
+++ models/post.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IPost extends Document {
+	title: string;
+	content: string;
+	user: Schema.Types.ObjectId;
+	comments: Schema.Types.ObjectId[];
+	published: boolean;
+}
+
+const PostSchema = new Schema(
+	{
+		title: { type: String, required: true, minLength: 1, maxLength: 40 },
+		content: { type: String, required: true, minLength: 1 },
+		user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment', required: true }],
+		published: { type: Boolean, required: true, default: false },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Post = mongoose.model<IPost>('Post', PostSchema);
+export default Post;
