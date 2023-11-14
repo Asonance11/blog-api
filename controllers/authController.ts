24c986893d4095c9e8defaef07cb094ce221abcd
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-14 07:13:00 +0100

    Add login functionality to authController

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,7 +1,9 @@
 import bcrypt from 'bcryptjs';
 import { NextFunction, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import User from '../models/user';
+import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import User, { IUser } from '../models/user';
 
 export const signup = [
 	body('username')
@@ -22,14 +24,13 @@ export const signup = [
 			}
 		}),
 	async (req: Request, res: Response, next: NextFunction) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			res.json({ errors: errors.array() });
-			return;
-		}
-
 		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				res.status(400).json({ errors: errors.array() });
+				return;
+			}
 			const userExists = await User.findOne({ username: req.body.username });
 
 			if (userExists) {
@@ -42,15 +43,70 @@ export const signup = [
 			const user = new User({
 				username: req.body.username,
 				password: hashedPassword,
-				member: false,
-				admin: false,
 			});
 
 			await user.save();
 
 			res.status(201).json({ message: 'User created' });
 		} catch (error: any) {
-			return res.status(500).json({ message: error.message.toString() });
+			console.error(error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	},
+];
+
+export const login = [
+	body('username')
+		.trim()
+		.isLength({ min: 3 })
+		.escape()
+		.withMessage('Username must consist of at least 3 characters'),
+	body('password')
+		.trim()
+		.isLength({ min: 6 })
+		.escape()
+		.withMessage('Password must consist of at least 6 characters'),
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				res.status(400).json({ errors: errors.array() });
+				return;
+			}
+			passport.authenticate(
+				'local',
+				{ session: false },
+				(err: Error | null, user: IUser, info: any) => {
+					if (err) {
+						res.status(500).json({ message: 'Error authenticating user' });
+						return;
+					}
+
+					if (!user) {
+						res.status(400).json({ message: 'Username or password incorrect' });
+						return;
+					}
+
+					req.login(user, (loginErr) => {
+						if (loginErr) {
+							res.status(500).json({ message: 'Error logging in user' });
+							return;
+						}
+
+						const token = jwt.sign(
+							{ id: user._id, username: user.username },
+							process.env.JWT_SECRET as string,
+							{ expiresIn: '1d' }
+						);
+
+						res.status(200).json({ message: 'User logged in', token });
+					});
+				}
+			)(req, res, next);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: 'Internal server error' });
 		}
 	},
 ];
