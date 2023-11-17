0e430f2a707f6ddd0b0f0a67de580b84ee16852d
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-17 15:03:06 +0100

    Add comment creation functionality

---
+++ controllers/commentController.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import Comment from '../models/comment';
+import { IUser } from '../models/user';
+
+export const createComment = [
+	body('comment')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Comment must not be empty'),
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				res.status(400).json({ errors: errors.array() });
+				return;
+			}
+
+			const user = req.user as IUser | undefined;
+
+			if (!user) {
+				res.status(401).json({ message: 'Unauthorized' });
+				return;
+			}
+
+			const { comment } = req.body;
+
+			const newComment = new Comment({
+				comment,
+				user: user._id,
+				post: req.params.postid,
+			});
+
+			await newComment.save();
+
+			res
+				.status(201)
+				.json({ message: 'Comment created successfully', newComment });
+		} catch (error: any) {
+			console.error(error);
+
+			if (error.name === 'ValidationError') {
+				return res
+					.status(400)
+					.json({ message: 'Validation error', errors: error.errors });
+			}
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	},
+];
